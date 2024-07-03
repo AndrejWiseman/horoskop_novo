37a52f15967d1382fbcdf4ecac0f41518595e3a6
Andrej <533737@gmail.com>
2024-07-03 13:50:02 +0200

    spinner

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
+from datetime import datetime
 from horoskop import ovan, bik, blizanci, rak, lav, devica, vaga, skorpija, strelac, jarac, vodolija, ribe
 
 
 app = Flask(__name__)
 
 
+@app.context_processor
+def inject_now():
+    return {'now': datetime.now()}
+
+
+@app.route('/znak/<znak>')
+def prikazi_znak(znak):
+    with open('data/horoskop.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    if znak in data:
+        return render_template('znak.html', znak=data[znak])
+    else:
+        return "Znak nije pronađen."
+
+
 @app.route('/')
+
 def index():
     return render_template('index.html')
 
