a9731f1f6017a76066c025100636cde059906d05
Andrej <533737@gmail.com>
2024-07-03 10:37:07 +0200

    3

--- app.py
+++ app.py
@@ -1,21 +1,73 @@
 from flask import Flask, render_template
-from horoskop import blizanci, vodolija
+from horoskop import ovan, bik, blizanci, rak, lav, devica, vaga, skorpija, strelac, jarac, vodolija, ribe
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+
+
+@app.route('/ovan')
+def ov():
+    return ovan()
+
+
+@app.route('/bik')
+def bi():
+    return bik()
 
 
 @app.route('/blizanci')
 def bliz():
     return blizanci()
 
+
+@app.route('/rak')
+def ra():
+    return rak()
+
+
+@app.route('/lav')
+def la():
+    return lav()
+
+
+@app.route('/devica')
+def dev():
+    return devica()
+
+
+@app.route('/vaga')
+def va():
+    return vaga()
+
+
+@app.route('/skorpija')
+def sko():
+    return skorpija()
+
+
+@app.route('/strelac')
+def str():
+    return strelac()
+
+
+@app.route('/jarac')
+def jar():
+    return jarac()
+
 @app.route('/vodolija')
 def vodo():
     return vodolija()
+
+@app.route('/ribe')
+def ri():
+    return ribe()
 
 
 if __name__ == '__main__':
--- horoskop.py
+++ horoskop.py
@@ -3,75 +3,231 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
 
+app = Flask(__name__)
+
 def fetch_horoscope(url):
     source = requests.get(url).text
     soup = BeautifulSoup(source, 'lxml')
     return soup.find('div', class_='article-details').text
 
+def get_horoscope_data(znak, urls):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(fetch_horoscope, urls))
+
+    dnevni, nedeljni, mesecni, godisnji = results
+
+    return render_template('horoskop-detaljno.html', znak=znak, dnevni=dnevni, nedeljni=nedeljni, mesecni=mesecni, godisnji=godisnji)
+
+
+@app.route('/ovan')
+def ovan():
+    znak = 'Ovan'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/ovan/',
+        'http://www.horoskopius.com/nedeljni-horoskop/ovan/',
+        'http://www.horoskopius.com/mesecni-horoskop/ovan/',
+        'http://www.horoskopius.com/godisnji-horoskop/ovan/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+@app.route('/bik')
+def bik():
+    znak = 'Bik'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/bik/',
+        'http://www.horoskopius.com/nedeljni-horoskop/bik/',
+        'http://www.horoskopius.com/mesecni-horoskop/bik/',
+        'http://www.horoskopius.com/godisnji-horoskop/bik/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+
+@app.route('/blizanci')
 def blizanci():
+    znak = 'Blizanci'
     urls = [
         'http://www.horoskopius.com/dnevni-horoskop/blizanci/',
         'http://www.horoskopius.com/nedeljni-horoskop/blizanci/',
         'http://www.horoskopius.com/mesecni-horoskop/blizanci/',
         'http://www.horoskopius.com/godisnji-horoskop/blizanci/'
     ]
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(fetch_horoscope, urls)
-
-    dnevni, nedeljni, mesecni, godisnji = results
-
-    return render_template('blizanci.html', dnevni=dnevni, nedeljni=nedeljni, mesecni=mesecni, godisnji=godisnji)
+    return get_horoscope_data(znak, urls)
 
 
 
+@app.route('/rak')
+def rak():
+    znak = 'Rak'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/rak/',
+        'http://www.horoskopius.com/nedeljni-horoskop/rak/',
+        'http://www.horoskopius.com/mesecni-horoskop/rak/',
+        'http://www.horoskopius.com/godisnji-horoskop/rak/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+
+@app.route('/lav')
+def lav():
+    znak = 'Lav'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/lav/',
+        'http://www.horoskopius.com/nedeljni-horoskop/lav/',
+        'http://www.horoskopius.com/mesecni-horoskop/lav/',
+        'http://www.horoskopius.com/godisnji-horoskop/lav/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+
+@app.route('/devica')
+def devica():
+    znak = 'Devica'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/devica/',
+        'http://www.horoskopius.com/nedeljni-horoskop/devica/',
+        'http://www.horoskopius.com/mesecni-horoskop/devica/',
+        'http://www.horoskopius.com/godisnji-horoskop/devica/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+
+
+@app.route('/vaga')
+def vaga():
+    znak = 'Vaga'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/vaga/',
+        'http://www.horoskopius.com/nedeljni-horoskop/vaga/',
+        'http://www.horoskopius.com/mesecni-horoskop/vaga/',
+        'http://www.horoskopius.com/godisnji-horoskop/vaga/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+
+
+@app.route('/skorpija')
+def skorpija():
+    znak = 'Skorpija'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/skorpija/',
+        'http://www.horoskopius.com/nedeljni-horoskop/skorpija/',
+        'http://www.horoskopius.com/mesecni-horoskop/skorpija/',
+        'http://www.horoskopius.com/godisnji-horoskop/skorpija/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+@app.route('/strelac')
+def strelac():
+    znak = 'Strelac'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/strelac/',
+        'http://www.horoskopius.com/nedeljni-horoskop/strelac/',
+        'http://www.horoskopius.com/mesecni-horoskop/strelac/',
+        'http://www.horoskopius.com/godisnji-horoskop/strelac/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+
+@app.route('/jarac')
+def jarac():
+    znak = 'Jarac'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/jarac/',
+        'http://www.horoskopius.com/nedeljni-horoskop/jarac/',
+        'http://www.horoskopius.com/mesecni-horoskop/jarac/',
+        'http://www.horoskopius.com/godisnji-horoskop/jarac/'
+    ]
+    return get_horoscope_data(znak, urls)
+
+
+@app.route('/vodolija')
 def vodolija():
+    znak = 'Vodolija'
     urls = [
         'http://www.horoskopius.com/dnevni-horoskop/vodolija/',
         'http://www.horoskopius.com/nedeljni-horoskop/vodolija/',
         'http://www.horoskopius.com/mesecni-horoskop/vodolija/',
         'http://www.horoskopius.com/godisnji-horoskop/vodolija/'
     ]
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(fetch_horoscope, urls)
-
-    dnevni, nedeljni, mesecni, godisnji = results
-
-    return render_template('vodolija.html', dnevni=dnevni, nedeljni=nedeljni, mesecni=mesecni, godisnji=godisnji)
+    return get_horoscope_data(znak, urls)
 
 
-
-
-
-
-
-
+@app.route('/ribe')
+def ribe():
+    znak = 'Ribe'
+    urls = [
+        'http://www.horoskopius.com/dnevni-horoskop/ribe/',
+        'http://www.horoskopius.com/nedeljni-horoskop/ribe/',
+        'http://www.horoskopius.com/mesecni-horoskop/ribe/',
+        'http://www.horoskopius.com/godisnji-horoskop/ribe/'
+    ]
+    return get_horoscope_data(znak, urls)
 
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+# import concurrent.futures
 # from flask import Flask, render_template
 # from bs4 import BeautifulSoup
 # import requests
 #
+# app = Flask(__name__)
 #
-# def blizanci():
-#     source = requests.get('http://www.horoskopius.com/dnevni-horoskop/blizanci/').text
-#     source_n = requests.get('http://www.horoskopius.com/nedeljni-horoskop/blizanci/').text
-#     source_m = requests.get('http://www.horoskopius.com/mesecni-horoskop/blizanci/').text
-#     source_g = requests.get('http://www.horoskopius.com/godisnji-horoskop/blizanci/').text
-#
+# def fetch_horoscope(url):
+#     source = requests.get(url).text
 #     soup = BeautifulSoup(source, 'lxml')
-#     soup_n = BeautifulSoup(source_n, 'lxml')
-#     soup_m = BeautifulSoup(source_m, 'lxml')
-#     soup_g = BeautifulSoup(source_g, 'lxml')
+#     return soup.find('div', class_='article-details').text
 #
-#     dnevni = soup.find('div', class_='article-details').text
-#     nedeljni = soup_n.find('div', class_='article-details').text
-#     mesecni = soup_m.find('div', class_='article-details').text
-#     godisnji = soup_g.find('div', class_='article-details').text
+# def get_horoscope_data(znak, urls):
+#     with concurrent.futures.ThreadPoolExecutor() as executor:
+#         results = executor.map(fetch_horoscope, urls)
 #
-#     return render_template('vodolija.html', dnevni=dnevni, nedeljni=nedeljni, mesecni=mesecni, godisnji=godisnji)
+#     dnevni, nedeljni, mesecni, godisnji = results
 #
+#     return render_template('horoskop-detaljno.html', znak=znak, dnevni=dnevni, nedeljni=nedeljni, mesecni=mesecni, godisnji=godisnji)
+#
+# @app.route('/blizanci')
+# def blizanci():
+#     znak = 'Blizanci'
+#     urls = [
+#         'http://www.horoskopius.com/dnevni-horoskop/blizanci/',
+#         'http://www.horoskopius.com/nedeljni-horoskop/blizanci/',
+#         'http://www.horoskopius.com/mesecni-horoskop/blizanci/',
+#         'http://www.horoskopius.com/godisnji-horoskop/blizanci/'
+#     ]
+#     return get_horoscope_data(znak, urls)
+#
+# @app.route('/vodolija')
+# def vodolija():
+#     znak = 'Vodolija'
+#     urls = [
+#         'http://www.horoskopius.com/dnevni-horoskop/vodolija/',
+#         'http://www.horoskopius.com/nedeljni-horoskop/vodolija/',
+#         'http://www.horoskopius.com/mesecni-horoskop/vodolija/',
+#         'http://www.horoskopius.com/godisnji-horoskop/vodolija/'
+#     ]
+#     return get_horoscope_data(znak, urls)
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
 #
 #
